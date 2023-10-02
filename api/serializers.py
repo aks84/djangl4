c976fa60269ff14fd8a25550d966af5df0761982
aks84 <amulyaa_24@yahoo.in>
2023-10-02 17:25:01 +0530

    added serializers and views

--- api/serializers.py
+++ api/serializers.py
@@ -12,3 +12,76 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+        
+
+class RestaurantsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Restaurants
+        fields = '__all__'
+
+
+class EmployeesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Employees
+        fields = '__all__'
+
+
+class CustomersSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customers
+        fields = '__all__'
+
+
+class MenuItemsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MenuItems
+        fields = '__all__'
+
+
+class OrdersSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Orders
+        fields = '__all__'
+
+
+class OrderDetailsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OrderDetails
+        fields = '__all__'
+
+
+class InventorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+
+class ThirdPartyPlatformsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ThirdPartyPlatforms
+        fields = '__all__'
+
+
+class ThirdPartyOrdersSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ThirdPartyOrders
+        fields = '__all__'
+
+
+class TableReservationsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TableReservations
+        fields = '__all__'
+
+
+class PaymentsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Payments
+        fields = '__all__'
+
+
+class ReviewsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Reviews
+        fields = '__all__'
+
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from api.serializers import UserSerializer, GroupSerializer
+from api.serializers import UserSerializer, GroupSerializer, RestaurantsSerializer, EmployeesSerializer, CustomersSerializer, MenuItemsSerializer, OrdersSerializer, OrderDetailsSerializer, InventorySerializer, ThirdPartyPlatformsSerializer, ThirdPartyOrdersSerializer, TableReservationsSerializer, PaymentsSerializer, ReviewsSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +20,76 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class Restaurants(viewsets.ModelViewSet):
+    queryset = Restaurants.objects.all()
+    serializer_class = RestaurantsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class Employees(viewsets.ModelViewSet):
+    queryset = Employees.objects.all()
+    serializer_class = EmployeesSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class Customers(viewsets.ModelViewSet):
+    queryset = Customers.objects.all()
+    serializer_class = CustomersSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MenuItems(viewsets.ModelViewSet):
+    queryset = MenuItems.objects.all()
+    serializer_class = MenuItemsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class Orders(viewsets.ModelViewSet):
+    queryset = Orders.objects.all()
+    serializer_class = OrdersSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class OrderDetails(viewsets.ModelViewSet):
+    queryset = OrderDetails.objects.all()
+    serializer_class = OrderDetailsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class Inventory(viewsets.ModelViewSet):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ThirdPartyPlatforms(viewsets.ModelViewSet):
+    queryset = ThirdPartyPlatforms.objects.all()
+    serializer_class = ThirdPartyPlatformsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ThirdPartyOrders(viewsets.ModelViewSet):
+    queryset = ThirdPartyOrders.objects.all()
+    serializer_class = ThirdPartyOrdersSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TableReservations(viewsets.ModelViewSet):
+    queryset = TableReservations.objects.all()
+    serializer_class = TableReservationsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class Payments(viewsets.ModelViewSet):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class Reviews(viewsets.ModelViewSet):
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
